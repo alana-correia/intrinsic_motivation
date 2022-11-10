0cfc4b0ae69a87a15083babd5bac9141aa4b8477
alana santana <alanasantana@MacBook-Air-de-alana.local>
2022-11-10 10:52:02 -0300

    train

--- test_script_curiosity.py
+++ test_script_curiosity.py
@@ -656,7 +656,7 @@ def function_with_args_and_default_kwargs(unk, **kwargs):
 
 if __name__ == "__main__":
     #args = parse_args()
-    run_name = "MsPacman-v4__my_baseline_intrinsic__1__1664396744"
+    run_name = "BreakoutNoFrameskip-v4__cnn_brims_mlp_mlp_hibrid_reward__1__1658017900"
     checkpoint_path = os.path.join("checkpoints", f"{run_name}_args.json")
     print(checkpoint_path)
     path_load = "checkpoints/"
